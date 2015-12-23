7fb516268af6aacc7bed8884a3baf93d5778beaf
ststeiger <quandary82@hailmail.net>
2015-12-23 14:07:14 +0100

    Initial commit

---
+++ NullableImage/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace NullableImage
+{
+
+
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [System.STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+
+
+    }
+
+
+}
